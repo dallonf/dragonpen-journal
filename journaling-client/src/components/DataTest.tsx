bb0e0425e24453bbdf412085dca542c5648676d7
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-16 16:13:18 -0500

    Use Apollo typegen

--- journaling-client/src/components/DataTest.tsx
+++ journaling-client/src/components/DataTest.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
+import { DataTestQuery } from './__generated__/DataTestQuery';
 
-const COUNTER = gql`
-  query DataTest {
+const DATA_TEST_QUERY = gql`
+  query DataTestQuery {
     counter
   }
 `;
 
 const DataTest = () => {
-  const { loading, error, data } = useQuery(COUNTER);
+  const { loading, error, data } = useQuery<DataTestQuery>(DATA_TEST_QUERY);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -18,7 +19,7 @@ const DataTest = () => {
     return <div style={{ color: 'red' }}>Error: {error.message}</div>;
   }
 
-  return <div>Counter: {data.counter}</div>;
+  return <div>Counter: {data?.counter}</div>;
 };
 
 export default DataTest;
