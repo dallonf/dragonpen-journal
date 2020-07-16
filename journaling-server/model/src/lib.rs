29aa1fd77ad9ecd339947361aa1cf182c8a9d32b
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-16 14:29:04 -0500

    A stateful model

---
+++ journaling-server/model/src/lib.rs
@@ -0,0 +1,35 @@
+use std::sync;
+
+pub struct ModelState {
+    pub counter: sync::Arc<sync::atomic::AtomicUsize>,
+}
+impl ModelState {
+    pub fn new() -> Self {
+        ModelState {
+            counter: sync::Arc::new(sync::atomic::AtomicUsize::new(0)),
+        }
+    }
+
+    pub fn get_counter(&self) -> usize {
+        self.counter.load(sync::atomic::Ordering::Relaxed)
+    }
+
+    pub fn increment_counter(&self) -> usize {
+        self.counter.fetch_add(1, sync::atomic::Ordering::Relaxed) + 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let state = ModelState::new();
+        assert_eq!(state.get_counter(), 0);
+        assert_eq!(state.increment_counter(), 1);
+        assert_eq!(state.get_counter(), 1);
+        assert_eq!(state.increment_counter(), 2);
+        assert_eq!(state.get_counter(), 2);
+    }
+}
