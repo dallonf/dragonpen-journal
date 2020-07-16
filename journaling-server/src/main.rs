03a5d945fe57d15424333ecafa7b3ecb2708782d
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-16 13:19:40 -0500

    Working HTTP server

--- journaling-server/src/main.rs
+++ journaling-server/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+async fn index() -> impl actix_web::Responder {
+    "Hello world!"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| actix_web::App::new().route("/", actix_web::web::get().to(index)))
+        .bind("127.0.0.1:4000")?
+        .run()
+        .await
 }
