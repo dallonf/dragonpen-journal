f3926e5454e967585209a52ad076ad0f0fe50fda
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-24 16:56:21 -0500

    Initial CDK commit

---
+++ cdk/bin/journaling.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { JournalingStack } from '../lib/journaling-stack';
+
+const app = new cdk.App();
+new JournalingStack(app, 'JournalingStack');
