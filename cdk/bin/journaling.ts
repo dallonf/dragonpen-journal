e4dc10ebe2962a8c9bf456b6f5b4dda6a4f8ec88
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-24 17:51:36 -0500

    ElasticSearch domain

--- cdk/bin/journaling.ts
+++ cdk/bin/journaling.ts
@@ -4,4 +4,4 @@ import * as cdk from '@aws-cdk/core';
 import { JournalingStack } from '../lib/journaling-stack';
 
 const app = new cdk.App();
-new JournalingStack(app, 'JournalingStack');
+new JournalingStack(app, 'JournalingStackStaging');
--- cdk/lib/journaling-stack.ts
+++ cdk/lib/journaling-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
+import * as elasticsearch from '@aws-cdk/aws-elasticsearch';
 
 export class JournalingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new elasticsearch.CfnDomain(this, 'elasticsearch', {
+      // TODO: VPC
+      elasticsearchClusterConfig: {
+        instanceCount: 1,
+        instanceType: 't2.small.elasticsearch',
+        dedicatedMasterEnabled: false,
+      },
+      elasticsearchVersion: '7.4',
+      ebsOptions: {
+        ebsEnabled: true,
+        volumeSize: 10,
+        volumeType: 'standard',
+      },
+    });
   }
 }
