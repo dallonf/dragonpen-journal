316040abcfb9494e6ba69ce58db294b3451842cd
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-27 10:17:16 -0500

    Dockerfile for server

--- journaling-server/src/model/elastic-client.ts
+++ journaling-server/src/model/elastic-client.ts
@@ -2,8 +2,7 @@ import { Client } from '@elastic/elasticsearch';
 
 export const makeClient = () => {
   const client = new Client({
-    // TODO: need to parameterize this for deployment
-    node: 'http://localhost:9200',
+    node: process.env.ELASTIC_NODE,
   });
   return client;
 };
--- journaling-server/src/types.d.ts
+++ journaling-server/src/types.d.ts
@@ -2,8 +2,11 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
+    // Identifier of Auth0 API for this app
     readonly AUTH0_IDENTIFIER: string;
     readonly AUTH0_DOMAIN: string;
+    // URL to ElasticSearch node
+    readonly ELASTIC_NODE: string;
     readonly REPL_USER_ID?: string;
   }
 }
