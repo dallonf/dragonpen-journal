1dff56d64f39476530a2ffe91eee49a0588fd563
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-30 16:48:40 -0500

    Flow to grab a JWT in development environment

--- journaling-server/index.ts
+++ journaling-server/index.ts
@@ -1,6 +1,8 @@
 import * as http from 'http';
+import * as qs from 'querystring';
 import type * as expressTypes from 'express';
 import { ApolloServer, AuthenticationError } from 'apollo-server-express';
+import axios from 'axios';
 import { typeDefs, resolvers, Context } from './src/schema';
 import createModel from './src/model';
 import { validateTokenAndGetUser } from './src/server/checkJwt';
@@ -9,8 +11,42 @@ const express = require('express') as () => expressTypes.Express;
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.json({ healthy: true });
 });
+
+// TODO: this might be better for env
+const baseUrl = 'http://localhost:4000';
+
+if (process.env.NODE_ENV === 'development') {
+  app.get('/jwt', (req, res, next) => {
+    (async () => {
+      const code = req.param('code');
+
+      if (code) {
+        const response = await axios.post(
+          `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+          qs.encode({
+            grant_type: 'authorization_code',
+            client_id: process.env.AUTH0_TEST_CLIENT_ID,
+            client_secret: process.env.AUTH0_TEST_CLIENT_SECRET,
+            redirect_uri: `${baseUrl}/jwt`,
+            code,
+          }),
+          {}
+        );
+        res.send(`Bearer ${response.data.access_token}`);
+      } else {
+        const url = `https://${process.env.AUTH0_DOMAIN}/authorize?${qs.encode({
+          audience: process.env.AUTH0_IDENTIFIER,
+          response_type: 'code',
+          client_id: process.env.AUTH0_TEST_CLIENT_ID,
+          redirect_uri: `${baseUrl}/jwt`,
+        })}`;
+        res.redirect(url);
+      }
+    })().catch(next);
+  });
+}
 
 const apolloServer = new ApolloServer({
   typeDefs,
--- journaling-server/src/types.d.ts
+++ journaling-server/src/types.d.ts
@@ -2,11 +2,16 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    // Identifier of Auth0 API for this app
+    readonly NODE_ENV: 'development' | 'production';
+    /** Identifier of Auth0 API for this app */
     readonly AUTH0_IDENTIFIER: string;
     readonly AUTH0_DOMAIN: string;
-    // URL to ElasticSearch node
+    /** URL to ElasticSearch node */
     readonly ELASTIC_NODE: string;
     readonly REPL_USER_ID?: string;
+    /** For /jwt flow. Only used in development. */
+    readonly AUTH0_TEST_CLIENT_ID?: string;
+    /** For /jwt flow. Only used in development. */
+    readonly AUTH0_TEST_CLIENT_SECRET?: string;
   }
 }
