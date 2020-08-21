b5c8ed35980774856552d5fd11e258d681802fc1
Dallon Feldner <dallon.feldner@gmail.com>
2020-08-21 15:37:55 -0500

    New env project

---
+++ env/index.js
@@ -0,0 +1,59 @@
+require('dotenv/config');
+const fs = require('fs');
+const path = require('path');
+const yup = require('yup');
+
+const envSchema = yup.object().shape({
+  ENV_NAME: yup.string().required(),
+
+  DEBUG: yup.string(),
+
+  REPL_USER_ID: yup.string(),
+  AUTH0_TEST_CLIENT_ID: yup.string(),
+  AUTH0_TEST_CLIENT_SECRET: yup.string(),
+});
+
+const sourceEnv = envSchema.cast(process.env);
+
+console.log('Collecting environment information');
+
+const DOMAIN_NAME = 'dallonf.com';
+const { ENV_NAME, DEBUG } = process.env;
+const hostPrefix = ENV_NAME == 'production' ? null : ENV_NAME;
+
+const getDomainName = (...names) =>
+  ['journal', ...names, hostPrefix ?? null].filter((x) => x).join('-') +
+  '.' +
+  DOMAIN_NAME;
+
+const getDynamoTableName = (name) => `${name}-${ENV_NAME}`;
+
+const apiDomain = getDomainName('api');
+const appDomain = getDomainName();
+
+const dynamoTableNames = Object.fromEntries(
+  ['JournalEntries'].map((x) => [x, getDynamoTableName(x)])
+);
+
+const output = {
+  auth0Domain: 'dallonf.auth0.com',
+  auth0ClientId: 'njBUh8oOFZe099w5nkxY0IqFY8aHO1O1',
+  auth0ApiId: 'https://api.journal.dallonf.com',
+  route53HostedZoneDomain: DOMAIN_NAME,
+  route53HostedZoneId: 'Z2T9RQTFXUZ2GU',
+  httpsCertArn:
+    'arn:aws:acm:us-east-1:784929213598:certificate/3c1b588a-eb2a-4fb5-9f0c-7113796c9884',
+
+  appDomain,
+  appUrl: `https://${appDomain}`,
+  apiDomain,
+  gqlUrl: `https://${apiDomain}/graphql`,
+
+  dynamoTableNames,
+};
+
+if (DEBUG) {
+  console.log(JSON.stringify(output, null, 2));
+} else {
+  fs.writeFileSync(path.join(__dirname, 'env.json'), JSON.stringify(output));
+}
