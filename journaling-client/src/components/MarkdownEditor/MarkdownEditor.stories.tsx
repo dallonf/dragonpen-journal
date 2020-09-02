8ed9113fbad3d3f7206eb7a1d5dd05a1f275bf38
Dallon Feldner <dallon.feldner@gmail.com>
2020-09-02 09:37:01 -0500

    Set up a playground for building a markdown editor

---
+++ journaling-client/src/components/MarkdownEditor/MarkdownEditor.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+export default {
+  title: 'MarkdownEditor',
+} as Meta;
+
+const Interactive: Story<{}> = () => <div>hello storybook</div>;
+
+export { Interactive };
