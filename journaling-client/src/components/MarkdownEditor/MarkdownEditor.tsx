72c4be7329318aaa4547a26ad2964a4417c63ff9
Dallon Feldner <dallon.feldner@gmail.com>
2020-09-05 17:19:56 -0500

    Added md-syntax marks

--- journaling-client/src/components/MarkdownEditor/MarkdownEditor.tsx
+++ journaling-client/src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
-import { schema } from 'prosemirror-schema-basic';
+import { schema as basicSchema } from 'prosemirror-schema-basic';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { undo, redo, history } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
 import { baseKeymap } from 'prosemirror-commands';
+import { Schema, MarkSpec } from 'prosemirror-model';
+import type OrderedMap from 'orderedmap';
+import styled from '@emotion/styled/macro';
+
+const MarkdownSourceStylesContainer = styled.div`
+  white-space: pre;
+
+  .md-syntax {
+    opacity: 0.5;
+  }
+`;
 
 const MarkdownEditor = () => {
   const containerRef = React.useCallback<(ref: HTMLPreElement | null) => void>(
     (ref) => {
       if (ref) {
+        const schema = new Schema({
+          nodes: basicSchema.spec.nodes,
+          marks: (basicSchema.spec.marks as OrderedMap<MarkSpec>).addToEnd(
+            'mdSyntax',
+            {
+              inclusive: false,
+              toDOM: (mark) => ['span', { class: 'md-syntax' }, 0],
+            }
+          ),
+        });
+
         const document = schema.node('doc', undefined, [
           schema.node('paragraph', undefined, [
-            schema.text("Hello "),
+            schema.text('Hello '),
+            schema.text('**', [schema.mark('mdSyntax')]),
             schema.text('world', [schema.mark('strong')]),
+            schema.text('**', [schema.mark('mdSyntax')]),
             schema.text(", I'm doing Markdown "),
+            schema.text('_', [schema.mark('mdSyntax')]),
             schema.text('stuff', [schema.mark('em')]),
-            schema.text("!"),
+            schema.text('_', [schema.mark('mdSyntax')]),
+            schema.text('!'),
           ]),
         ]);
 
@@ -47,7 +73,11 @@ const MarkdownEditor = () => {
     []
   );
 
-  return <pre ref={containerRef} />;
+  return (
+    <MarkdownSourceStylesContainer>
+      <pre ref={containerRef} />
+    </MarkdownSourceStylesContainer>
+  );
 };
 
 export default MarkdownEditor;
