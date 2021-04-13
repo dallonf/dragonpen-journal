4e8d07d76708b4430a4b9f50c80d638a536a0b6b
Dallon Feldner <dallon.feldner@gmail.com>
2021-04-13 09:05:56 -0500

    Upgrade CDK

--- cdk/lib/journaling-lambda.ts
+++ cdk/lib/journaling-lambda.ts
@@ -1,11 +1,12 @@
-import * as path from 'path';
-import * as cdk from '@aws-cdk/core';
-import * as lambdaNode from '@aws-cdk/aws-lambda-nodejs';
-import * as apiGateway from '@aws-cdk/aws-apigatewayv2';
-import * as route53 from '@aws-cdk/aws-route53';
-import * as acm from '@aws-cdk/aws-certificatemanager';
-import { EnvConfig } from './env';
-import { JournalingDBStack } from './journaling-db-stack';
+import * as path from "path";
+import * as cdk from "@aws-cdk/core";
+import * as lambdaNode from "@aws-cdk/aws-lambda-nodejs";
+import * as apiGateway from "@aws-cdk/aws-apigatewayv2";
+import * as apiGatewayIntegrations from "@aws-cdk/aws-apigatewayv2-integrations";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as acm from "@aws-cdk/aws-certificatemanager";
+import { EnvConfig } from "./env";
+import { JournalingDBStack } from "./journaling-db-stack";
 
 interface JournalingLambdaProps {
   envConfig: EnvConfig;
@@ -33,8 +34,8 @@ export class JournalingLambda extends cdk.Construct {
 
     const apiDomain = props.envConfig.apiDomain;
 
-    const serverDir = path.join(__dirname, '../../journaling-server');
-    const handlersDir = path.join(serverDir, 'src/handlers');
+    const serverDir = path.join(__dirname, "../../journaling-server");
+    const handlersDir = path.join(serverDir, "src/handlers");
 
     const environment = {
       AUTH0_IDENTIFIER: props.envConfig.auth0ApiId,
@@ -42,42 +43,42 @@ export class JournalingLambda extends cdk.Construct {
       DYNAMO_TABLE_NAMES: JSON.stringify(props.envConfig.dynamoTableNames),
     };
 
-    const graphql = new lambdaNode.NodejsFunction(this, 'gqlFn', {
-      entry: path.join(handlersDir, 'gql.ts'),
+    const graphql = new lambdaNode.NodejsFunction(this, "gqlFn", {
+      entry: path.join(handlersDir, "gql.ts"),
       environment,
     });
 
     props.dbStack.tables.JournalEntries.grantReadWriteData(graphql);
 
-    const domain = new apiGateway.DomainName(this, 'domain', {
+    const domain = new apiGateway.DomainName(this, "domain", {
       domainName: apiDomain,
       certificate: props.acmCert,
     });
 
-    const api = new apiGateway.HttpApi(this, 'gqlApi', {
+    const api = new apiGateway.HttpApi(this, "gqlApi", {
       defaultDomainMapping: { domainName: domain },
       corsPreflight: {
-        allowHeaders: ['Authorization', 'Content-Type'],
+        allowHeaders: ["Authorization", "Content-Type"],
         allowMethods: [
-          apiGateway.HttpMethod.GET,
-          apiGateway.HttpMethod.POST,
-          apiGateway.HttpMethod.HEAD,
-          apiGateway.HttpMethod.OPTIONS,
+          apiGateway.CorsHttpMethod.GET,
+          apiGateway.CorsHttpMethod.POST,
+          apiGateway.CorsHttpMethod.HEAD,
+          apiGateway.CorsHttpMethod.OPTIONS,
         ],
-        allowOrigins: ['*'],
+        allowOrigins: ["*"],
         maxAge: cdk.Duration.days(10),
       },
     });
 
     api.addRoutes({
-      path: '/graphql',
+      path: "/graphql",
       methods: [apiGateway.HttpMethod.POST],
-      integration: new apiGateway.LambdaProxyIntegration({
+      integration: new apiGatewayIntegrations.LambdaProxyIntegration({
         handler: graphql,
       }),
     });
 
-    new route53.ARecord(this, 'apiRecord', {
+    new route53.ARecord(this, "apiRecord", {
       zone: props.hostedZone,
       recordName: apiDomain,
       target: route53.RecordTarget.fromAlias(new ApiGatewayV2Target(domain)),
