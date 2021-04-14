4b6fd30b68be4f24df6f3e704e63a98575891eed
Dallon Feldner <dallon.feldner@gmail.com>
2021-04-13 19:27:10 -0500

    Debug deploy

--- journaling-client/scripts/deploy.js
+++ journaling-client/scripts/deploy.js
@@ -1,32 +1,33 @@
-const path = require('path');
-const childProcess = require('child_process');
-const env = require('../src/env.json');
-const deployOut = require('../../cdk/deploy-output.json');
+const path = require("path");
+const childProcess = require("child_process");
+const env = require("../src/env.json");
+const deployOut = require("../../cdk/deploy-output.json");
 
 const stack = deployOut[`JournalingStack-${env.envName}`];
+console.log("Deploy Out:", deployOut);
 const { uiBucket: bucket, uiDistribution: cfDistribution } = stack;
 console.assert(
   bucket,
-  'Expected Bucket to exist in JSON: ' + JSON.stringify(deployOut)
+  "Expected Bucket to exist in JSON: " + JSON.stringify(deployOut)
 );
 
 console.log(`Syncing with ${bucket}...`);
 
 childProcess.execSync(
-  `aws s3 sync ${path.join(__dirname, '..', 'build')} ${bucket} --delete`,
+  `aws s3 sync ${path.join(__dirname, "..", "build")} ${bucket} --delete`,
   {
-    stdio: 'inherit',
+    stdio: "inherit",
   }
 );
 
-console.log('UI is deployed!');
+console.log("UI is deployed!");
 
-console.log('Invalidating distribution cache...');
+console.log("Invalidating distribution cache...");
 childProcess.execSync(
   `aws cloudfront create-invalidation --distribution-id ${cfDistribution} --paths "/*" --output text`,
   {
-    stdio: 'inherit',
+    stdio: "inherit",
   }
 );
 
-console.log('Done!');
+console.log("Done!");
