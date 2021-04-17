106a21abb45f1392fe3638a9f7d719245735cefd
Dallon Feldner <dallon.feldner@gmail.com>
2021-04-17 17:13:48 -0500

    Install Emotion Babel plugin

--- journaling-client/vite.config.ts
+++ journaling-client/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import { babel } from "@rollup/plugin-babel";
 import env from "./src/env.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    babel({
+      babelHelpers: "bundled",
+      include: ["src/**/*"],
+      plugins: ["emotion"],
+      extensions: [".ts", ".tsx"],
+    }),
+  ],
   server: {
     proxy: {
       "/graphql": {
