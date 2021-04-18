8316d4d59195223bc5d7df491a701d00ad4ad99c
Dallon Feldner <dallon.feldner@gmail.com>
2021-04-18 09:09:27 -0500

    build works on Vite now

--- journaling-client/vite.config.ts
+++ journaling-client/vite.config.ts
@@ -1,4 +1,3 @@
-import * as path from "path";
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { babel } from "@rollup/plugin-babel";
@@ -6,11 +5,8 @@ import env from "./src/env.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      tslib: path.resolve(__dirname, "src/empty.ts"),
-      "@material-ui/icons": "@material-ui/icons/esm",
-    },
+  build: {
+    outDir: "build",
   },
   plugins: [
     reactRefresh(),
