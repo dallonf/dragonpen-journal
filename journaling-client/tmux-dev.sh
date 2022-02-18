b4604605ef794210abfb16176383652057f99adb
Dallon Feldner <dallon.feldner@gmail.com>
2022-02-17 19:28:30 -0600

    GraphQL codegen update

--- journaling-client/tmux-dev.sh
+++ journaling-client/tmux-dev.sh
@@ -7,6 +7,6 @@ tmux new-session \; \
   \
   send-keys 'npm run typecheck -- --watch' C-m \; \
   select-pane -t 1 \; \
-  send-keys 'npm run typegen:gql -- --watch' C-m \; \
+  send-keys 'npm run typegen:gql:watch' C-m \; \
   select-pane -t 2 \; \
   send-keys 'npm run start' C-m \; \
